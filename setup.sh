0a62fc73acf78b37092e7f0236c31cc3bf5d52b0
Hikmat Farhat <62365718+hikmatfarhat-ndu@users.noreply.github.com>
2021-03-01 07:45:43 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,6 +10,7 @@ suffix=$(echo "$1" |cut -d '/' -f 2 |grep 'homework')
 if [ "$suffix" != "" ]
 then
   echo $token |gh auth login --with-token
+  gh auth status
   gh secret set workflow_token -b "'$token'" -R "$1"
 else
   echo  "not homework"
